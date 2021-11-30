08e98a8cdc5d179b007df9caefd5e282497280b2
ART <pakkawat.kh@gmail.com>
2021-12-01 02:03:21 +0700

    fix error in wholeday data

--- fe_detailwholeday.php
+++ fe_detailwholeday.php
@@ -4,21 +4,26 @@ $_POST = json_decode(file_get_contents("php://input"),true);
 $id=$_POST['id'];
 $startTime=$_POST['startTime'];
 $endTime=$_POST['endTime'];
-// $id=5;
-// $startTime=1638378000000;
-// $endTime=1640365200000;
+// $id=1;
+// $startTime=1635699600000;
+// $endTime=1669827600000;
+if($id <=2){
+    $prefix = "SG0";
+} else {
+    $prefix = "SG";
+}
 $col[0]=($id-1)*5+1;
-$col[0]="SG" . $col[0] . "Max";
+$col[0]=$prefix . $col[0] . "Max";
 $col[1]=($id-1)*5+2;
-$col[1]="SG" . $col[1] . "Max";
+$col[1]=$prefix . $col[1] . "Max";
 $col[2]=($id-1)*5+3;
-$col[2]="SG" . $col[2] . "Max";
+$col[2]=$prefix . $col[2] . "Max";
 $col[3]=($id-1)*5+4;
-$col[3]="SG" . $col[3] . "Max";
+$col[3]=$prefix . $col[3] . "Max";
 $col[4]=($id-1)*5+5;
-$col[4]="SG" . $col[4] . "Max";
+$col[4]=$prefix . $col[4] . "Max";
 $sql="select ".$col[0] ." , ".$col[1]. " , " .$col[2]. " , " .$col[3]. " , " .$col[4] . " , timestamp from alldaydata where timestamp between ". $startTime. " and " .$endTime. " order by timestamp";
-
+// echo $sql;
 //print_r($col);
 // $sql="select"
 $result = $db->query($sql)->fetchAll();
